6f72e296f705f312fb1ebed13be4561a79397229
ОДНО.КАСАНИЕ Разработчики <dev@odnokasanie.ru>
2019-06-16 04:55:11 +0300

    get_osm_data

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.post("/get_osm_data/:paramId", async (req, res) => {
   console.debug('/get_osm_data', req.body.osmIds);
   const osmData = await storage.getOsmData(req.body.osmIds,req.params.paramId);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(JSON.stringify(osmData));
   res.json(osmData);
 });
 
